4eafc73a3381bb9e30b5e9cdf4636e28b6ceadf5
fady dib <fady.dib@hotmail.com>
2024-09-10 12:15:04 +0300

    Add tailwind css

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store  from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
