30c5710bc8dfbe8435fbf68ff4eb822a65feb1b6
fady dib <fady.dib@hotmail.com>
2024-09-15 17:48:38 +0300

    Add header

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,21 @@
 
 function home() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <div className="shadow-md shadow-gray-100">
+                    <header className="flex justify-between items-center py-5 lato-bold text-[20px] container mx-auto px-5">
+                            <div className="flex items-center">
+                                <img src="images/fady_portfolio.svg" className="h-[72px] w-[72px] rounded-full object-cover"></img>
+                                <p className="pl-5">Fady Dib</p>
+                            </div>
+                        <div className="flex gap-16 text-[16px]">
+                            <p>HOME</p>
+                            <p>ABOUT</p>
+                            <p>PROJECTS</p>
+                            <p>CONTACT</p>
+                        </div>
+                    </header>
+            </div>
     )
 }
 
