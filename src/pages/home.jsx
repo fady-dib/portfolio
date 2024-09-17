d89a0b90a2445b60c663191f1a7d57c1e89e9330
fady dib <fady.dib@hotmail.com>
2024-09-17 16:05:35 +0300

    Fix header and home content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,20 @@
+import Portfolio from "../components/Portfolio/Portfolio";
+import {portfolio} from "../utils/data"
 function home() {
+
     const handleProjectsClick = () => {
         console.log(1)
     }
+
     return (
         <>
-            <div className="shadow-md shadow-gray-100">
+            <section id="home" className="common-bg ">
+                <div className="shadow-md shadow-gray-300 z-1000 bg-white">
                     <header className="flex justify-between items-center py-5 lato-bold text-[20px] container mx-auto px-5">
-                            <div className="flex items-center">
-                                <img src="images/fady_portfolio.svg" className="h-[72px] w-[72px] rounded-full object-cover"></img>
-                                <p className="pl-5">Fady Dib</p>
-                            </div>
+                        <div className="flex items-center">
+                            <img src="images/fady_portfolio.svg" className="h-[72px] w-[72px] rounded-full object-cover"></img>
+                            <p className="pl-5">Fady Dib</p>
+                        </div>
                         <div className="flex gap-16 text-[16px]">
                             <p>HOME</p>
                             <p>ABOUT</p>
@@ -17,12 +22,17 @@ function home() {
                             <p>CONTACT</p>
                         </div>
                     </header>
-            </div>
-            <section id="home" className="common-bg">
-                <div className="text-center">
-                    <p>HEY, I'M FADY DIB</p>
-                    <p>A Result-Oriented Web Developer building and managing Websites and Web Applications that leads to the success of the overall product</p>
-                    <button onClick={handleProjectsClick()}>PROJECTS</button>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <div className=" flex flex-col justify-center items-center pt-60 pb-48">
+                        <p className="lato-black text-[60px] leading-none">HEY, I'M FADY DIB</p>
+                        <p className="text-center max-w-[800px] text-[22px] py-12">A Result-Oriented Web Developer building and managing Websites and Web Applications that lead to the success of the overall product</p>
+                        <button className=" bg-[#7843E9] duration-700 hover:-translate-y-1 lato-black text-white py-4 px-20 rounded text-[20px]" onClick={handleProjectsClick()}>PROJECTS</button>
+                    </div>
+                    <div className="py-16">
+                        <div className="mouse">
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
