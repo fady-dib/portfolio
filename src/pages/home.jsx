6b1d12764a36c7458d5288f81ffec0bc34ed3e58
fady dib <fady.dib@hotmail.com>
2024-12-06 16:45:52 +0200

    Add container padding to all sections

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -57,9 +57,9 @@ function Home() {
                         </div>
                     )}
                 </div>
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col justify-center items-center mx-auto px-5 container">
                     <div className=" flex flex-col justify-center items-center pt-60 pb-48">
-                        <p className="lato-black text-[60px] leading-none">HEY, I'M FADY DIB</p>
+                        <p className="lato-black text-[60px] leading-none text-center">HEY, I'M FADY DIB</p>
                         <p className="text-center max-w-[800px] text-[22px] py-12">A Result-Oriented Web Developer building and managing Websites and Web Applications that lead to the success of the overall product</p>
                         <button className=" bg-[#7843E9] duration-700 hover:-translate-y-1 lato-black text-white py-4 px-20 rounded text-[20px]" onClick={handleProjectsClick()}>PROJECTS</button>
                     </div>
@@ -69,41 +69,47 @@ function Home() {
                     </div>
                 </div>
             </section>
-            <section id="about" className="pt-16 pb-20 bg-gray-100">
-                <p className="lato-black text-center text-[35px] relative pb-4">ABOUT ME <span className="underline-part"></span></p>
-                <p className="text-[#555] text-center pt-4 text-[20px] max-w-[700px] mx-auto">Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
-                <About portfolio={portfolio}></About>
-                <div className="grid grid-cols-1 lg:grid-cols-12 gap-20 pt-20 container mx-auto px-5">
-                    <div className="lg:col-span-6">
-                        <p className="lato-bold text-[30px]">Get to know me!</p>
-                        <div className="text-[#666] text-[18px]">
-                            <p className="pt-10 mb-5">I am a <span className="lato-bold">Full Stack Web Developer</span> with a focus on building and managing both the front-end and back-end of websites and web applications, contributing to the overall success of the product. Check out some of my work in the <span className="lato-bold">Projects</span> section.</p>
-                            <p>Feel free to connect or follow me on LinkedIn and Github. I'm open to <span className="lato-bold">job</span> opportunities where I can contribute, learn, and grow. If you have a role that aligns with my skills and experience, don't hesitate to <span className="lato-bold">reach out.</span></p>
+            <section id="about" className="pt-16 pb-20 bg-gray-100 ">
+                <div className="mx-auto px-5 container">
+                    <p className="lato-black text-center text-[35px] relative pb-4">ABOUT ME <span className="underline-part"></span></p>
+                    <p className="text-[#555] text-center pt-4 text-[20px] max-w-[700px] mx-auto">Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+                    <About portfolio={portfolio}></About>
+                    <div className="grid grid-cols-1 lg:grid-cols-12 gap-20 pt-20">
+                        <div className="lg:col-span-6">
+                            <p className="lato-bold text-[30px]">Get to know me!</p>
+                            <div className="text-[#666] text-[18px]">
+                                <p className="pt-10 mb-5">I am a <span className="lato-bold">Full Stack Web Developer</span> with a focus on building and managing both the front-end and back-end of websites and web applications, contributing to the overall success of the product. Check out some of my work in the <span className="lato-bold">Projects</span> section.</p>
+                                <p>Feel free to connect or follow me on LinkedIn and Github. I'm open to <span className="lato-bold">job</span> opportunities where I can contribute, learn, and grow. If you have a role that aligns with my skills and experience, don't hesitate to <span className="lato-bold">reach out.</span></p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="lg:col-span-6">
-                        <p className="lato-bold text-[30px]">My skills</p>
-                        <div className="flex flex-wrap gap-4 pt-10">
-                            {skills.map((skill, index) => (
-                                <div
-                                    key={index}
-                                    className="bg-gray-200 text-center py-2 px-4 rounded-lg shadow-md"
-                                >
-                                    {skill}
-                                </div>
-                            ))}
+                        <div className="lg:col-span-6">
+                            <p className="lato-bold text-[30px]">My skills</p>
+                            <div className="flex flex-wrap gap-4 pt-10">
+                                {skills.map((skill, index) => (
+                                    <div
+                                        key={index}
+                                        className="bg-gray-200 text-center py-2 px-4 rounded-lg shadow-md"
+                                    >
+                                        {skill}
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
             <section id="projects" className="pt-16 pb-20 bg-[#fffff]">
-                <p className="lato-black text-center text-[35px] relative pb-4">PROJECTS <span className="underline-part"></span></p>
-                <p className="text-[#555] text-center pt-4 text-[20px] max-w-[700px] mx-auto">Here you will find some of the projects that I worked on</p>
+                <div className="mx-auto px-5 container">
+                    <p className="lato-black text-center text-[35px] relative pb-4">PROJECTS <span className="underline-part"></span></p>
+                    <p className="text-[#555] text-center pt-4 text-[20px] max-w-[700px] mx-auto">Here you will find some of the projects that I worked on</p>
+                </div>
                 <Projects projects={projects}></Projects>
             </section>
             <section id="contact" className="pt-16 pb-20 common-bg">
-                <p className="lato-black text-center text-[35px] relative pb-4">CONTACT <span className="underline-part"></span></p>
-                <p className="text-[#555] text-center pt-4 text-[20px] max-w-[700px] mx-auto">Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
+                <div className="mx-auto px-5 container">
+                    <p className="lato-black text-center text-[35px] relative pb-4">CONTACT <span className="underline-part"></span></p>
+                    <p className="text-[#555] text-center pt-4 text-[20px] max-w-[700px] mx-auto">Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
+                </div>
                 <Contact></Contact>
             </section>
             <Footer></Footer>
