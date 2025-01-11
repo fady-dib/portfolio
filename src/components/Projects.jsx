2ae86298b3af4251da9c7d7decd5bb098f067e09
fady dib <fady.dib@hotmail.com>
2025-01-11 18:30:38 +0200

    C projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,98 +2,87 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import arrow_left from '../assets/images/left-arrow-back-svgrepo-com.svg'
-import arrow_right from '../assets/images/right-arrow-next-svgrepo-com.svg'
 
-
-const Projects = ({projects}) => {
-
-   const SamplePrevArrow = (props) => {
-        const { className, style, onClick } = props;
-        return (
-            <div onClick={onClick} className={`arrow ${className}`} >
-                <img src={arrow_left} className='arrows' alt="ARROW LEFT"></img>
-            </div>
-        )
-    }
-
-    function SampleNextArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div onClick={onClick} className={`arrow ${className}`} >
-                <img src={arrow_right} className='arrows' alt="ARROW RIGHT"></img>
-            </div>
-        )
-    }
-
+const Projects = ({ projects }) => {
     const sliderSettings = {
         autoplay: false,
         dots: true,
-        arrows: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: <SampleNextArrow to="next" />,
-        prevArrow: <SamplePrevArrow to="prev" />,
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    arrows: false
-                },
-            },
-            {
-                breakpoint: 1050,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: true,
-                    arrows: false
-                },
-            },
-            {
-                breakpoint: 650,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false
-
-                },
-            },
-        ],
+        arrows: true,
     };
 
-  return (
-    <>
-          <Slider {...sliderSettings} className="slider flex flex-wrap justify-center items-center mb-5 pr-4 pt-20 mx-auto px-5 container">
-              {projects.map((project,index) => (
-                  <div key={index} className={`item custom:px-6 flex flex-col`}>
-                      <div className="w-64 h-64">
-                      {project.url ? (
-                          <a href={project.url} target="_blank" rel="noopener noreferrer">
-                              <img
-                                src={project.image}
-                                className="w-full h-full object-scale-down carousel-item"
-                                alt={project.title}
-                              />
-                          </a>
-                      ) : (
-                          <img
-                            src={project.image}
-                            className="w-full h-full object-scale-down carousel-item"
-                            alt={project.title}
-                          />
-                      )}
-                      </div>
-                      <p className="mt-2">{project.title}</p>
+    const groupedProjects = [];
+    for (let i = 0; i < projects.length; i += 6) {
+        groupedProjects.push(projects.slice(i, i + 6));
+    }
 
-                  </div>
-              ))}
-          </Slider>
-    </>
-  )
-}
+    return (
+        <div className="projects-container pt-20 mx-auto px-5 container">
+            <Slider {...sliderSettings} className="slider">
+                {groupedProjects.map((group, groupIndex) => (
+                    <div key={groupIndex} className="slide-group flex flex-col">
+                        <div className="row flex justify-center mb-4">
+                            {group.slice(0, 3).map((project, index) => (
+                                <div
+                                    key={index}
+                                    className="project-item w-[441px] h-[320px] flex flex-col m-6 bg-white rounded-[15px] items-center justify-center "
+                                >
+                                    <div className="image-wrapper">
+                                        {project.url ? (
+                                            <a href={project.url} target="_blank" rel="noopener noreferrer">
+                                                <img
+                                                    src={project.image}
+                                                    className="w-[256px] h-[256px] object-contain carousel-item"
+                                                    alt={project.title}
+                                                />
+                                            </a>
+                                        ) : (
+                                            <img
+                                                src={project.image}
+                                                className="w-full h-full object-scale-down carousel-item"
+                                                alt={project.title}
+                                            />
+                                        )}
+                                    </div>
+                                    <p className="mt-2 text-center">{project.title}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="row flex justify-center">
+                            {group.slice(3, 6).map((project, index) => (
+                                <div
+                                    key={index}
+                                    className="project-item w-64 h-64 flex flex-col m-2"
+                                >
+                                    <div className="image-wrapper">
+                                        {project.url ? (
+                                            <a href={project.url} target="_blank" rel="noopener noreferrer">
+                                                <img
+                                                    src={project.image}
+                                                    className="w-full h-full object-scale-down carousel-item"
+                                                    alt={project.title}
+                                                />
+                                            </a>
+                                        ) : (
+                                            <img
+                                                src={project.image}
+                                                className="w-full h-full object-scale-down carousel-item"
+                                                alt={project.title}
+                                            />
+                                        )}
+                                    </div>
+                                    <p className="mt-2 text-center">{project.title}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+};
 
-export default Projects
+export default Projects;
